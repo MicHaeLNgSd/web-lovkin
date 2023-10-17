17d9257fa9f4094d50d810322e3367fa9ddc2e59
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2023-10-17 11:08:15 +0300

    lr7_prretier

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,11 +1,19 @@
 function burger() {
-  const burgerMenu = document.getElementById("burgerMenu");
-  const burgerBtnOpen = document.getElementById("burgerBtnOpen");
-  const burgerBtnClose = document.getElementById("burgerBtnClose");
+  console.log("burgerToggle");
 
-  console.log("burgerToggle")
-
-  burgerMenu.classList.toggle("invisible");
-  burgerBtnOpen.classList.toggle("invisible");
-  burgerBtnClose.classList.toggle("invisible");
+  $("#burgerMenu").toggleClass("invisible");
+  $("#burgerBtnOpen").toggleClass("invisible");
+  $("#burgerBtnClose").toggleClass("invisible");
 }
+
+// function burger() {
+//   const burgerMenu = document.getElementById("burgerMenu");
+//   const burgerBtnOpen = document.getElementById("burgerBtnOpen");
+//   const burgerBtnClose = document.getElementById("burgerBtnClose");
+
+//   console.log("burgerToggle")
+
+//   burgerMenu.classList.toggle("invisible");
+//   burgerBtnOpen.classList.toggle("invisible");
+//   burgerBtnClose.classList.toggle("invisible");
+// }
